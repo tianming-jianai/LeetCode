81edcd8cbe54bcc80fd4235be6f0676775f8734d
zsg1001 <1724473740@qq.com>
2022-07-23 23:08:31 +0800

    项目搭建

---
+++ src/test/java/com/zsg/LeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
